984f44e783ebc2e4e6867d8d062d0e42c946740e
Aditya Shrivastav <167913271+Audatic07@users.noreply.github.com>
2026-07-07 16:08:10 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) return nums[0];
+        int prev2=nums[0]; //best answer when we have 1 house
+        int prev1=max(nums[0],nums[1]); //best answer when we have 2 houses, that is max between the two
+        for(int i=2; i<nums.size(); i++){
+            int current = max(prev1,prev2+nums[i]); //rob it or not, choosing the max option here.
+            prev2=prev1;
+            prev1=current;
+        }
+        return prev1;
+    }
+};
